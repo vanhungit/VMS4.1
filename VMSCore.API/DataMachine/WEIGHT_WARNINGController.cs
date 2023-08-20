a521907e90ef0cde9c5c4db7a5c8f2b662298153
Hung Hung <daylaptrinhnangcao@gmail.com>
2023-08-20 12:36:48 +0700

    post

---
+++ VMSCore.API/DataMachine/WEIGHT_WARNINGController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VMSCore.API.EntityModels.Models;
+using VMSCore.Infrastructure.Base.Repositories;
+
+namespace VMSCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WEIGHT_WARNINGController : BaseApiController<WEIGHT_WARNING>
+    {
+        public WEIGHT_WARNINGController(BaseRepositoryCore<WEIGHT_WARNING> repository) : base(repository)
+        {
+
+        }
+    }
+}
